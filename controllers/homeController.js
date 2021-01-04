4a8df349bbbe6113f02a9f4d4d0ee4c449f71662
Sirwan <sr222qn@student.lnu.se>
2021-01-04 21:04:41 +0100

    Added function to get issues through gitlab Rest api

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,24 @@
-const index = (req, res) => {
-    res.send("Hello from Controller")
+const request = require('request')
+
+
+const index = (req, response) => {
+    
+    const options1 = {
+        url: 'https://gitlab.lnu.se/api/v4/projects/10737/',
+        json: true
+      }
+      request.get(options1, async (err, res, html) => {
+  
+        if (!err && res.statusCode === 200) {
+            const viewData = {
+                name: html[0].author.name,
+                username: html[0].author.username 
+              }
+          
+              response.render('custom/home', { viewData})
+        }
+      })
+  
   }
   
   module.exports = { index }
